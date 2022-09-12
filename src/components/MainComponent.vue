01e77740b879c542742a2726a89c262f0dac7947
giusscos <giu.cosenza08@gmail.com>
2022-09-12 11:31:31 +0200

    Aggiunta poster Milestone 3

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,11 @@
 <template>
     <main>
         <ul>
+            <li>
+                <h3>
+                    Film
+                </h3>
+            </li>
             <li v-for="movie in movieFilter" :key="movie.id">
                 Titolo: {{ movie.title }}<br>
                 Titolo originale: {{ movie.original_title }}<br>
@@ -8,6 +13,12 @@
                     :src="`https://flagsapi.com/${movie.original_language.toUpperCase()}/flat/64.png`"
                     :alt="`Bandiera > ${movie.original_language}`" /> <br>
                 Voto: {{ movie.vote_average }}<br>
+                Poster: <img :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`" :alt="`Poster de: ${movie.original_title}`"/>
+            </li>
+            <li>
+                <h3>
+                    Serie Tv
+                </h3>
             </li>
             <li v-for="tv in tvsFilter" :key="tv.id">
                 Nome: {{ tv.name }} <br>
@@ -16,6 +27,7 @@
                     :src="`https://flagsapi.com/${tv.original_language.toUpperCase()}/flat/64.png`"
                     :alt="`Bandiera > ${tv.original_language}`" /> <br>
                 Voto: {{ tv.vote_average }}<br>
+                Poster: <img :src="`https://image.tmdb.org/t/p/w200${tv.poster_path}`" :alt="`Poster de: ${tv.original_title}`"/>
             </li>
         </ul>
     </main>
